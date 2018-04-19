124d33a7194aa24d17aee2ad406bdf7444144b1a
sevi221 <sevilaura221@gmail.com>
2018-04-19 21:35:51 +0200

    last commit for deploy

--- src/app/shared/alarm/alarm.component.ts
+++ src/app/shared/alarm/alarm.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { User } from './../model/user.model';
 import { Timer } from './../model/timer.model';
-import { TimerService } from './../service/timer.service'
 import { Alarm } from './../model/alarm.model';
-
+import { TimerService } from './../service/timer.service';
 
 @Component({
   selector: 'app-alarm',
@@ -11,8 +10,36 @@ import { Alarm } from './../model/alarm.model';
   styleUrls: ['./alarm.component.css']
 })
 export class AlarmComponent implements OnInit {
+  alarm: Alarm = new Alarm;
+  timer: Timer = new Timer;
+  sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/clocks/clock_chime.wav");
 
-  constructor() { }
+  constructor() {}
+
+  getTime(){
+    const date = new Date();
+    this.timer.hours = this.formatTime(date.getHours());
+    this.timer.minutes = this.formatTime(date.getMinutes());
+    this.timer.seconds = this.formatTime(date.getSeconds());
+    return this.timer;
+  }
+
+  formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  setAlarm(){
+    const date = new Date();
+      this.alarm.hours = this.formatTime(date.getHours());
+      this.alarm.minutes = this.formatTime(date.getMinutes());
+      this.alarm.seconds = this.formatTime(date.getSeconds());
+    return this.alarm;
+  }
+
+  playAlarm(){
+      if (this.timer == this.alarm)
+      return this.sound.play();
+    }
 
   ngOnInit() {
   }
